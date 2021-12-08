938314adea5eca710fa61364a3215ab9fa62e59f
AhmedA <alkhatib_ahmed1995@hotmail.com>
2021-12-08 17:34:58 -0500

    added first question

--- app.js
+++ app.js
@@ -1,70 +1,22 @@
-/*
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const inquirer = require('inquirer');
 
-const printProfileData = profileDataArr => {
-    // This ...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-    console.log("================");
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-    // Is the same as this ...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// const pageHTML = generatePage(name, github);
 
-printProfileData(profileDataArgs); */
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-
-// Commenting out code to keep as referance, refactoring all the code below!
-/*
-const fs = require('fs');
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-//first method                                               COMMENT
-// const name = profileDataArgs[0];                          COMMENT
-// const github = profileDataArgs[1];                        COMMENT
-
-// alternative to name and github seperate const             COMMENT
-const [name, github] = profileDataArgs;
-
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-     </head>
-
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>    
-    `;
-};
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw err;
-
-    console.log('Protfolio complete! Check out index.html to see the output!');
-});
-*/
-
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
